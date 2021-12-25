a6f224014b53ed36d61ade2daa8563f7d853fa19
ExxEss <yuguoxie@gmail.com>
2021-12-25 12:14:51 -0600

    Restore escape key

--- src/js/iframe.js
+++ src/js/iframe.js
@@ -1,3 +1,5 @@
+import Util from "./util";
+
 let video = null,
   neverPlayed = true;
 
@@ -11,10 +13,20 @@ new MutationObserver(() => {
         neverPlayed = false;
       }
     }
-  } catch (e) {
-    console.log(e);
+  } catch (error) {
+    console.log(error);
   }
 }).observe(document, { childList: true, subtree: true });
+
+window.addEventListener(
+  "keydown",
+  (e) => {
+    const activeElement = document.activeElement;
+    if (Util.isSelectable(activeElement)) return;
+    if (e.key === "Escape") parent.postMessage("removeVideo", "*");
+  },
+  true
+);
 
 window.onmessage = (e) => {
   if (e.data.focus === true) video.focus();
