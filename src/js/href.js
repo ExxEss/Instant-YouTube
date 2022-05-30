fe34cd35a1358a1c888689bc872b289cd5c1a115
ExxEss <yuguoxie@gmail.com>
2022-05-30 08:40:36 -0500

    Url bug fixed

--- src/js/href.js
+++ src/js/href.js
@@ -1,18 +1,33 @@
 import Browser from 'webextension-polyfill';
 
+const youtubeUrlPrefix =
+  'https://www.youtube.com/watch?v=';
+
 let previousHref;
 
 const youtubeUrlPattern =
   /www.youtube.com\/(watch)+/;
 
+const extractVideoId = (url) => {
+  const index = url.indexOf('v=') + 2;
+  let id = '';
+  for (let i = index; i < url.length; i++) {
+    if (url[i] === '&') return id;
+    id += url[i];
+  }
+  return id;
+}
+
 setInterval(() => {
   if (
     previousHref !== document.location.href &&
-    youtubeUrlPattern.test(document.location.href)) {
+    youtubeUrlPattern.test(document.location.href)
+  ) {
     previousHref = document.location.href;
     Browser.runtime.sendMessage({
       type: 'addHistory',
-      url: document.location.href,
+      url: youtubeUrlPrefix +
+        extractVideoId(document.location.href),
     });
   }
 }, 1000);
